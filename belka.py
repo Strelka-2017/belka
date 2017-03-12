eb2fddb8515982cc00a302cad524f989d7f6ddba
clevinson <cory@soundcloud.com>
2017-03-12 11:40:25 +0000

    we dont need no authentication

--- belka.py
+++ belka.py
@@ -13,10 +13,6 @@ logging.basicConfig(**{
     'level': logging.INFO
 })
 
-# We'll store the SlackClient instances for each team in a
-# dictionary, so we can have multiple teams authed
-CLIENTS = {}
-
 # Our app's Slack Event Adapter for receiving actions via the Events API
 SLACK_VERIFICATION_TOKEN = os.environ["SLACK_VERIFICATION_TOKEN"]
 slack_events_adapter = SlackEventAdapter(SLACK_VERIFICATION_TOKEN, "/slack/events")
@@ -34,50 +30,6 @@ ARENA_ACCESS_TOKEN = os.environ["ARENA_ACCESS_TOKEN"]
 ARENA_POST_URL = os.environ["ARENA_POST_URL"]
 
 CLIENT = SlackClient(SLACK_TEST_TOKEN)
-
-# This route renders the installation page with 'Add to Slack' button.
-@slack_events_adapter.server.route("/", methods=["GET"])
-def pre_install():
-    add_to_slack = """
-        <a href="https://slack.com/oauth/authorize?scope=bot&client_id=%s">
-            <img alt="Add to Slack" src="https://platform.slack-edge.com/img/add_to_slack.png"/>
-        </a>
-    """ % SLACK_CLIENT_ID
-    return add_to_slack
-
-
-# This route is called by Slack after the user installs our app. It will
-# exchange the temporary authorization code Slack sends for an OAuth token
-# which we'll save on the bot object to use later.
-# To let the user know what's happened it will also render a thank you page.
-@slack_events_adapter.server.route("/auth/slack/callback", methods=["GET"])
-def thanks():
-
-    # Get the OAuth code to pass into the token request
-    auth_code = request.args.get('code')
-
-    # Create a temporary Slack client to make the OAuth request.
-    # This client doesn't need a token.
-    slack_client = SlackClient("")
-
-    # Ask Slack for a bot token, using the auth code we received
-    auth_response = slack_client.api_call(
-        "oauth.access",
-        client_id=SLACK_CLIENT_ID,
-        client_secret=SLACK_CLIENT_SECRET,
-        code=auth_code
-    )
-
-    # Grab the user's team info and token from the OAuth response
-    team_id = auth_response.get("team_id")
-    team_name = auth_response.get("team_name")
-    bot_token = auth_response["bot"].get("bot_access_token")
-    logger.info("bot_token {}".format(bot_token))
-
-    # Create a SlackClient for your bot to use for Web API requests
-    CLIENTS[team_id] = SlackClient(bot_token)
-    return "Your app has been installed on <b>%s</b>" % team_name
-
 
 # Now we'll set up some event listeners for our app to process and respond to
 # Example responder to greetings
